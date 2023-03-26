28339a6615026b6fded3b7752e9f460bf18f9eb3
Shaurya <maverick.shaurya@live.com>
2023-03-26 10:04:25 +0530

    feat add persistence layer
    add StockDatabase.kt, StockDao.kt

--- app/src/main/java/com/example/playcompose/MainActivity.kt
+++ app/src/main/java/com/example/playcompose/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.playcompose.ui.theme.PlayComposeTheme
 
-const val API_KEY = "M9M5BZNOPJYGXDHX"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
